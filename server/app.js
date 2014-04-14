716cedfa41f5fb17288a5ed51c2eb8cf1ab6e2fc
Xiaoli Shen <alabebop@gmail.com>
2014-04-14 17:45:55 +0200

    change to work with express 4 according to https://gist.github.com/jeffscottward/10419517

--- server/app.js
+++ server/app.js
@@ -25,11 +25,11 @@ app.use(cookieParser());
 //app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
+});*/
 
 /// error handlers
 
@@ -47,13 +47,40 @@ app.use(function(req, res, next) {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
-});
+});*/
 
+/**
+ * Development Settings
+ */
+if('development' === app.get('env')) {
+	//This will change in production since we'll be using the dist folder
+	//This covers serving up the index page
+	app.use(express.static(path.join(__dirname, '../client/.tmp')));
+	app.use(express.static(path.join(__dirname, '../client/app')));
+	// This is the new way to handle errors in Express 4. not errorHandler().
+	// For more about error-first best practices see http://fredkschott.com/post/2014/03/understanding-error-first-callbacks-in-node-js/
+	app.use(function(err, req, res, next){
+		console.error(err.stack);
+		res.send(500, 'Something broke!');
+	});
+}
+
+/**
+ * Production Settings
+ */
+if('production' === app.get('env')) {
+	app.use(express.static(path.join(__dirname, '/dist')));
+}
+
+/* Add this to fire the server */
+var server = app.listen(3000, function() {
+	console.log('Listening on port %d', server.address().port);
+});
 
 module.exports = app;
